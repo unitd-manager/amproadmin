62b84112773ca9f5fba09b0c49f6b0164ac9430a
syed jasmine <smsaf91@gmail.com>
2025-05-15 16:58:47 +0530

    Initial commit

--- src/views/smartconTables/CpfSummaryReports.js
+++ src/views/smartconTables/CpfSummaryReports.js
@@ -133,6 +133,8 @@ function CpfSummaryReports() {
               <option value="2021">2021</option>
               <option value="2022">2022</option>
               <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
             </Input>
           </Col>
           <Col md="2">
--- src/views/smartconTables/PayslipGeneratedReports.js
+++ src/views/smartconTables/PayslipGeneratedReports.js
@@ -146,6 +146,7 @@ function PayslipGeneratedReports() {
               <option value="2022">2022</option>
               <option value="2023">2023</option>
               <option value="2024">2024</option>
+              <option value="2025">2025</option>
             </Input>
             </FormGroup>
           </Col>
