9c3a392b3e84586ca5ed42d4c6fbf9e5a0d1d97c
syed jasmine <smsaf91@gmail.com>
2025-07-29 19:47:32 +0530

    Message

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,11 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
 baseURL: 'https://amproadmin.zaitunsoftsolutions.com:2002',
-<<<<<<< HEAD
-// baseURL: 'http://localhost:2001',
-=======
 //baseURL: 'http://localhost:2001',
->>>>>>> addab87d14968b8801a4c2befc36570630f1fed8
 });
 
 
