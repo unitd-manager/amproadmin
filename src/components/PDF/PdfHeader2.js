cc4895352dc567c246393b66e31f64cc51930ed9
NabeelaAkram16 <nabeela@unitdtechnologies.com>
2026-01-20 20:15:07 +0530

    modified

---
+++ src/components/PDF/PdfHeader2.js
@@ -0,0 +1,56 @@
+import moment from 'moment';
+
+const PdfHeader = ({ findCompany }) => {
+  return function pdfHeaderContent(currentPage, pageCount) {
+    return {
+      margin: [40, 20, 40, 5],
+      stack: [
+        {
+          columns: [
+            {
+              text: findCompany('company_name') || 'AMPRO PTE LTD',
+              fontSize: 16,
+              bold: true,
+            },
+            {
+              text: `Print Date : ${moment().format('MM/DD/YYYY hh:mm:ss A')}`,
+              alignment: 'right',
+              fontSize: 9,
+            },
+          ],
+        },
+          {
+          columns: [
+        {
+          text: 'Invoice Packing List',
+          fontSize: 11,
+          bold: true,
+        },
+         {
+          text: `Page No : ${currentPage} / ${pageCount}`,
+          alignment: 'right',
+          fontSize: 9,
+         
+        },
+          ],
+            margin: [3, 8, 8, 0],
+        },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 0,
+              x2: 515,
+              y2: 0,
+              lineWidth: 1,
+            },
+          ],
+        },
+       
+      ],
+    };
+  };
+};
+
+export default PdfHeader;
